6092fc9bd07ca068221adca48b98f84a444e41c9
timcartwright <tim@websitesbytim.com>
2016-01-04 16:47:45 +0530

    Set default language from browser

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,8 +10,17 @@ class ApplicationController < ActionController::Base
   end
  
 private
+  def extract_locale_from_accept_language_header
+    http_accept_language = request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+    if ['fr', 'en'].include? http_accept_language
+      http_accept_language
+    else
+      'en'
+    end
+  end
+
   def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
+    I18n.locale = params[:locale] || extract_locale_from_accept_language_header || I18n.default_locale
     Rails.application.routes.default_url_options[:locale]= I18n.locale
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
 
-  scope ':locale', locale: /en|fr/ do
-    root to: 'home#land'
-    get "home/land"
-  end
+  # scope ':locale', locale: /en|fr/ do
+  #   root to: 'home#land'
+  #   get "home/land"
+  # end
 
   # root 'home#land'
   devise_for :users
+
+  scope ':locale', locale: /en|fr/ do
+    get "home/land"
+    get '/', to: 'home#land'
+  end
+
+  root 'home#land'
 end
