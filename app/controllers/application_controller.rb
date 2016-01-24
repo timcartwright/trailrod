158ec366f84f482cf8412b03a83316f7d06c4dd8
timcartwright <tim@websitesbytim.com>
2016-01-24 11:50:42 +0400

    Style form with materialize

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
   before_action :set_locale
+  layout 'admin', only: [:new, :edit]
 
   def default_url_options
     { locale: I18n.locale }
