8015c286e62894f4517b95e6ef4bf9d0219a6dfd
timcartwright <tim@websitesbytim.com>
2016-01-25 10:55:05 +0400

    Annotate

--- app/helpers/event_registrations_helper.rb
+++ app/helpers/event_registrations_helper.rb
@@ -1,2 +1,21 @@
+# == Schema Information
+#
+# Table name: event_registrations
+#
+#  id             :integer          not null, primary key
+#  event_id       :integer
+#  profile_id     :integer
+#  paid           :boolean
+#  amount         :decimal(, )
+#  currency       :string
+#  payment_date   :datetime
+#  payment_method :string
+#  transaction_id :string
+#  race_number    :integer
+#  race_category  :string
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
+
 module EventRegistrationsHelper
 end
--- test/models/event_registration_test.rb
+++ test/models/event_registration_test.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: event_registrations
+#
+#  id             :integer          not null, primary key
+#  event_id       :integer
+#  profile_id     :integer
+#  paid           :boolean
+#  amount         :decimal(, )
+#  currency       :string
+#  payment_date   :datetime
+#  payment_method :string
+#  transaction_id :string
+#  race_number    :integer
+#  race_category  :string
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
+
 require 'test_helper'
 
 class EventRegistrationTest < ActiveSupport::TestCase
