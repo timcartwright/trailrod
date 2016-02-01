75d30cdd3645e9ce9bd0f27839a9da0a6c5b69be
timcartwright <tim@websitesbytim.com>
2016-02-01 10:49:21 +0400

    Config for sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,16 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.smtp_settings = {
+    :address   => "smtp.sendgrid.net",
+    :port      => 587, # ports 587 and 2525 are also supported with STARTTLS
+    :enable_starttls_auto => true, # detects and uses STARTTLS
+    :user_name => ENV["SENDGRID_USERNAME"],
+    :password  => ENV["SENDGRID_PASSWORD"], # SMTP password is any valid API key
+    :authentication => 'login',
+    :domain => 'heroku.com', # your domain to identify your server when connecting
+  }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
