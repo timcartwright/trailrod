de459a90890154d506fe593ace31365a2e85a506
timcartwright <tim@websitesbytim.com>
2016-02-01 17:20:45 +0400

    Sign in redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,12 @@ class ApplicationController < ActionController::Base
   end 
 
   def after_sign_in_path_for(resource)
-    request.referrer
+    sign_in_url = new_user_session_url
+    if request.referer == sign_in_url
+      super
+    else
+      stored_location_for(resource) || request.referer || root_path
+    end
   end
 
   def after_sign_out_path_for(resource_or_scope)
