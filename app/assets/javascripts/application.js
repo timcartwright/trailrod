735a7280e5a1ffa107bc7ce5cb9625606d36a01c
timcartwright <tim@websitesbytim.com>
2016-03-05 21:47:40 +0400

    Fix animation in Firefox

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -64,6 +64,7 @@ $(document).ready(function() {
           var offset = elementOffset.left*0.8 + elementOffset.top;
           var delay = parseFloat(offset/speed).toFixed(2);
           $(this)
+            .css("-moz-animation-delay", delay+'s')
             .css("-webkit-animation-delay", delay+'s')
             .css("-o-animation-delay", delay+'s')
             .css("animation-delay", delay+'s')
