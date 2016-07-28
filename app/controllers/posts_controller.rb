00e1e22eff0ea11c2209db22016f338e4275df2b
timcartwright <tim@websitesbytim.com>
2016-07-28 16:18:34 +0100

    Dynamic post background colours

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,7 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all.order(created_at: :desc)
+    @post_background_colour = 0
   end
 
   def new
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,6 @@
 module PostsHelper
+  def tile_colour
+    colours = ['tile-green','tile-cyan','tile-deep-orange','tile-light-blue']
+    colours[@post_background_colour % 4]
+  end
 end
