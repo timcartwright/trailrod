2ed87399663445acddd4992dc659773cfbe31bf1
timcartwright <tim@websitesbytim.com>
2016-07-28 15:46:41 +0100

    Delete posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
 
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, except: [:index]
   before_action :find_post, only: [:edit, :update]
 
   def index
@@ -30,6 +30,11 @@ class PostsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path
   end
 
 private
