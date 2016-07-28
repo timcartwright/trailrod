d25283149487d1de9faacfc2b037b5555d510ed3
timcartwright <tim@websitesbytim.com>
2016-07-28 14:42:28 +0100

    Edit posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
 
   def index
-    @posts = Post.all
+    @posts = Post.all.order(created_at: :desc)
   end
 
   def new
@@ -13,22 +13,29 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     if @post.save
-      redirect_to post_path(@post)
+      redirect_to posts_path
     else
       render :new
     end
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(post_params)
+      redirect_to posts_path
+    else
+      render :edit
+    end
   end
 
 private
 
-  def trail_params
-    params.require(:post).permit(:image, :title, :body)
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 
 end
